b19b575f03c453b2e3206b9514309ba2a4b8ec9d
Efield44444235364 <efi3lx@gmail.com>
2025-06-04 22:10:27 +0700

    Update Loader.lua
    [ + ] loader

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,7 @@
+-- Loader.lua (อันนี้คือสคริปต์ที่คุณให้ไว้ปรับใหม่)
+
+_G.efield_loader = true  -- ตั้งตัวแปรบอกว่าโหลดผ่าน Loader
+
 local hasFpsCap = (type(setfpscap) == "function")
 
 if not hasFpsCap then
@@ -5,6 +9,7 @@ if not hasFpsCap then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/MoreUNC.lua"))()
 end
 
+-- โหลดสคริปต์หลักโดยให้สคริปต์หลักตรวจสอบ _G.efield_loader
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/When%20admin%20join%20server.lua"))()
 
 
