b88d6a737ea3ae2887cccbb0830dcd2dba4b892b
Efield44444235364 <efi3lx@gmail.com>
2025-06-04 17:08:39 +0700

    Create Notification.lua

---
+++ Notification.lua
@@ -0,0 +1,7 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "❌ Anti-Ban and Anti-Kick not active", -- หัวข้อ
+    Text = "These bypass methods are specifically designed and optimized for the "Blox Fruits" map only. Using them outside of this context may result in instability or detection, and is not recommended!!", -- ข้อความหลัก
+    Duration = 5 -- ระยะเวลาแสดงผล (วินาที)
+})
