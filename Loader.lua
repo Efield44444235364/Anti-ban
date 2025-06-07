f3d5c3f879d52867fce35ab970fc2dadf14070c5
Efield44444235364 <efi3lx@gmail.com>
2025-06-07 22:02:05 +0700

    Update Loader.lua
    [ + ] Add Anti ban Grow a Garden

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
--- Loader.lua (อันนี้คือสคริปต์ที่คุณให้ไว้ปรับใหม่)
 
-_G.efield_loader = true  -- ตั้งตัวแปรบอกว่าโหลดผ่าน Loader
+
+_G.efield_loader = true
 
 local hasFpsCap = (type(setfpscap) == "function")
 
@@ -9,17 +9,18 @@ if not hasFpsCap then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/MoreUNC.lua"))()
 end
 
--- โหลดสคริปต์หลักโดยให้สคริปต์หลักตรวจสอบ _G.efield_loader
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/When%20admin%20join%20server.lua"))()
 
+-- โหลดสคริปต์เฉพาะแมพ Grow a Garden เท่านั้น
+if game.PlaceId == 126884695634066 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/GaG%20Notification.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/Anti-Kick.lua"))()
+end
 
 if FPSOptimize == true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/BoostFPS.lua"))()
 end
 
-
-
--- Kick Online 
 while true do
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Anti-ban/refs/heads/main/KickOnline.lua"))()
 end
